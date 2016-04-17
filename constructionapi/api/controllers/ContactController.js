1a62fe6011c341a8abc5bc38290cf273e9d82074
m <m>
2016-04-17 13:23:43 +0100

    Add backend code

---
+++ constructionapi/api/controllers/ContactController.js
@@ -0,0 +1,16 @@
+/**
+ * ContactController
+ *
+ * @description :: Server-side logic for managing contacts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	restricted:function(req,res){
+		return res.ok("If You can see this you are authenticated");
+	},	
+	open:function(req,res){
+		return res.ok("This is open to all!!!");
+	}	
+};
+
