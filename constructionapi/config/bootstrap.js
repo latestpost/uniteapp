c9e9067401223cf3f145a2f979a7b49db0e5917b
Mike Griffin <Mike.Griffin@Rightmove.co.uk>
2016-04-18 13:28:24 +0100

    mock data for job rest api

--- constructionapi/config/bootstrap.js
+++ constructionapi/config/bootstrap.js
@@ -16,13 +16,24 @@ module.exports.bootstrap = function(cb) {
             email: 'test@test.com',
             password: 'testpassword'
         };
-        User.create(users, function locationsObj(err, locations) {
+        User.create(users, function Obj(err, locations) {
             if (err) {
                 cb(err);
             }
             console.log('users created: ', users);
         });
 
+        // seed the database with Jobs
+              var job = {
+                  title: 'A Job',
+                  description: 'Some job description.'
+              };
+              Job.create(job, function Obj(err, locations) {
+                  if (err) {
+                      cb(err);
+                  }
+                  console.log('jobs created: ', job);
+              });
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
