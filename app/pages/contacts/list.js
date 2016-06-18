28e846f1a4f584ac82bc704390dbca420097364f
Mike Griffin <mike.griffin@rightmove.co.uk>
2016-06-18 12:44:16 +0100

    Hook up contacts search to filter on last name

--- app/pages/contacts/list.js
+++ app/pages/contacts/list.js
@@ -1,27 +1,31 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
 import {ItemDetailsPage} from './item-details';
 import {RestService} from '../../services/RestService';
+import {SearchablePage} from '../../shared/SearchablePage';
 
 @Page({
   templateUrl: 'build/pages/contacts/list.html',
   providers: [RestService]
 })
-export class ContactsPage {
+export class ContactsPage extends SearchablePage {
   static get parameters() {
     return [[NavController], [NavParams], [RestService]];
   }
 
   constructor(nav, navParams, restService) {
+    super();
+
     this.restService = restService;
     this.nav = nav;
 
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+    this.filterField = "lastname";
   }
 
   ngOnInit() {
         this.restService.findUsers().subscribe(
-            data => this.contacts = data
+            data => this.items = this.filteredItems = data
         );
     }
 
